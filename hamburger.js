c0ca366f44efd6300c2027b92be360c4c6bd2cce
Bartosz Bartosiewicz <45470344+imbartek@users.noreply.github.com>
2019-10-07 12:30:54 +0200

    array error fixed

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav__links');
-const linkss = document.querySelectorAll('.nav__links li');
 const overlay = document.querySelector('.overlay');
 const links = document.querySelectorAll('.overlay-content a');
+const linksArray = Array.from(links);
 
 
 hamburger.addEventListener('click', () => {
@@ -10,8 +9,8 @@ hamburger.addEventListener('click', () => {
     overlay.classList.toggle('open');
 });
 
-for(i=0; i<=4; i++){
-    links[i].addEventListener('click', () => {
+for(let i=0; i<linksArray.length; i++){
+    linksArray[i].addEventListener('click', () => {
     hamburger.classList.remove('active');
     overlay.classList.remove('open');
 });
